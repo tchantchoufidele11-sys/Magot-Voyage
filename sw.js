df959ec796f764156c8b2d2b9f48a4f1efc3a653
Fidèle TL <tchantchoufidele11@gmail.com>
2026-08-25 15:03:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Magot Voyage — Service Worker */
-const VERSION = "v759";
+const VERSION = "v761";
 const CACHE = "magot-voyage-" + VERSION;
 const SHELL = [
   "./","./index.html","./studio.html","./manifest.json","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png"
