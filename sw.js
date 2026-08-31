9470ac8afc61b25eda3e8c6978a1cec4dd7281fb
Fidèle TL <tchantchoufidele11@gmail.com>
2026-08-31 19:00:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const VERSION = "v865";
+const VERSION = "v866";
 self.addEventListener('install', (e) => e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
